dceac2bed8ce1d3c748d9cde63e65db1ba3980c4
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2018-05-30 21:09:53 -0300

    Add new line on EOF

--- packages/airbnb-ui/src/card/Card.js
+++ packages/airbnb-ui/src/card/Card.js
@@ -7,21 +7,6 @@ const Container = styled.div`
   position: relative;
 `;
 
-// const Image = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   background-image: url(${props => props.src});
-//   background-repeat: no-repeat;
-//   background-size: contain;
-//   position: absolute;
-//   top: 0;
-// `;
-
-// const Info = styled.div`
-//     display: block;
-//     padding-top: 8px;
-// `;
-
 class Card extends Component {
   render() {
     return (
